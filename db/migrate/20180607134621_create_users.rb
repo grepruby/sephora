5d9b08da2647617feac10bef758f055dfb321f87
Nilesh Verma <NileshV@techracers.com>
2018-06-07 22:02:50 +0530

    api for users

---
+++ db/migrate/20180607134621_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.integer :gender
+      t.string :country
+      t.integer :loyalty_account_id
+
+      t.timestamps
+    end
+    add_index :users, :email
+    add_index :users, :loyalty_account_id
+  end
+end
